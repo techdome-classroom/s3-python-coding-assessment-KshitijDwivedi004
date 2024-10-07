c1b408ba291f024b8577de813bb506d853f3e0b1
Kshitij Dwivedi <117004290+KshitijDwivedi004@users.noreply.github.com>
2024-10-07 15:12:13 +0530

    Updated

--- program1.py
+++ program1.py
@@ -4,9 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
-
-
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+        
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        
+        return not stack
 
 
 
--- program2.py
+++ program2.py
@@ -1,10 +1,15 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
-
-
-
+        total = 0
+        prev_value = 0
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        
+        for char in reversed(s):
+            current_value = roman_map[char]
+            if current_value < prev_value:
+                total -= current_value
+            else:
+                total += current_value
+            prev_value = current_value
+        
+        return total
